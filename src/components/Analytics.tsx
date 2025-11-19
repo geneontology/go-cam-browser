79789a42dbe61d0ccc0e05abe98d71701dda41c2
Patrick Kalita <pkalita@lbl.gov>
2025-11-19 09:54:48 -0800

    Manually inject script tags for GA

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { config } from "../config.tsx";
 
 /**
@@ -5,36 +6,44 @@ import { config } from "../config.tsx";
  *
  * It only initializes analytics in production mode and if the googleTagID is set.
  *
- * Note: This component relies on support for document metadata tags introduced in React 19.
- * See also: https://react.dev/reference/react-dom/components/script
+ * Note: This component manually injects <script> tags into the document head to load Google
+ *       Analytics. It does not rely on Reach 19's built-in <script> tag handling because that
+ *       does not execute inline script content.
+ *       See:
+ *         - https://react.dev/reference/react-dom/components/script
+ *         - https://github.com/facebook/react/issues/34008
  */
 const Analytics = () => {
-  if (import.meta.env.DEV) {
-    console.warn("Skipping analytics setup in development mode");
-    return null;
-  }
-  if (!config.googleTagID) {
-    console.warn("Skipping analytics setup because googleTagID is not set");
-    return null;
-  }
-  return (
-    <>
-      <script
-        async={true}
-        src={`https://www.googletagmanager.com/gtag/js?id=${config.googleTagID}`}
-      />
-      <script>
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-              dataLayer.push(arguments);
-            }
-            gtag("js", new Date());
-            gtag("config", "${config.googleTagID}");
-        `}
-      </script>
-    </>
-  );
+  useEffect(() => {
+    if (import.meta.env.DEV) {
+      console.warn("Skipping analytics setup in development mode");
+      return;
+    }
+    if (!config.googleTagID) {
+      console.warn("Skipping analytics setup because googleTagID is not set");
+      return;
+    }
+    const srcScriptElement = document.createElement("script");
+    srcScriptElement.src = `https://www.googletagmanager.com/gtag/js?id=${config.googleTagID}`;
+    srcScriptElement.async = true;
+    document.head.appendChild(srcScriptElement);
+
+    const inlineScriptElement = document.createElement("script");
+    inlineScriptElement.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${config.googleTagID}');
+    `;
+    document.head.appendChild(inlineScriptElement);
+
+    return () => {
+      document.head.removeChild(srcScriptElement);
+      document.head.removeChild(inlineScriptElement);
+    };
+  }, []);
+
+  return null;
 };
 
 export default Analytics;
